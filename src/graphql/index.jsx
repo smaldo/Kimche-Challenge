ba0dec3c9bc653a2e677cfc7c19211606989b09a
smaldo <sergiomaldonadoolguin@gmail.com>
2022-03-24 14:25:39 -0300

    fix(graphql): elimina InMemoryCache

--- src/graphql/index.jsx
+++ src/graphql/index.jsx
@@ -1,6 +1,5 @@
 import ApolloClient from "apollo-boost";
 
 export const client = new ApolloClient({
-    uri: "https://countries.trevorblades.com/",
-    cache: new InMemoryCache()
+    uri: "https://countries.trevorblades.com/"
 });
